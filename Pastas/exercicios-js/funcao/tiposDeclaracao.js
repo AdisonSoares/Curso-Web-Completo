9d027dac2f0dddb8a9510baa69214d1e1242aea6
Adison Soares <adison.soares7@gmail.com>
2021-12-27 16:45:32 -0300

    Tipos de Declaracoes

---
+++ Pastas/exercicios-js/funcao/tiposDeclaracao.js
@@ -0,0 +1,14 @@
+//Declaração da função - Function declaration, posso chamar antes
+function soma(x, y){
+    return x + y
+}
+
+//Expressão de função - Function expression, posso chamar depois
+const sub = function(x, y){
+    return x - y
+}
+
+//Nomeação da expressão de função - Named function expression, posso chamar depois
+const mult = function mult(x, y){
+    return x * y
+}
