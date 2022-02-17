fbf77af20a0eedba4d07c201504c69bda1f45209
Adison Soares <62489704+AdisonSoares@users.noreply.github.com>
2022-02-17 13:33:14 -0300

    2_Funcao - 1_Estudo do escopo.

--- Estudo/1_JavaScript/2_Funcao/Closure.js
+++ Estudo/1_JavaScript/2_Funcao/Closure.js
@@ -1,5 +1,5 @@
 /*Closure é o escopo criado no momento de declaração de uma função,
-permite à função acessar e manipular variáveis externas à função,ela é
+permite à função acessar e manipular variáveis externas à função, ela é
 o contexto lexico em ação.*/
 
 const x = 'Global'
