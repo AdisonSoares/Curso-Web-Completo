06b718688318791c8fd9700b6c8dd2fc4f66645d
Adison Soares <adison.soares7@gmail.com>
2022-02-26 10:15:51 -0300

    4_Objeto - 3_Heranca.

--- Estudo/1_JavaScript/4_Objeto/Heranca03.js
+++ Estudo/1_JavaScript/4_Objeto/Heranca03.js
@@ -1,27 +1,43 @@
-const carro = {
-    velAtual = 0,
-    velMax = 200,
-    acelerarMais(delta){
-        if(this.velAtual + delta <= this.velMax){
-            this.velAtual += delta
-        }else{
-            this.velAtual=this.velMax
-        }
-    },
-    status(){
-        return `${this.velAtual}Km/h de ${this.velMax}Km/h`
+/*Uso do Object.create*/
+
+const pai = {
+    nome: 'Adriano',
+    corCabelo: 'preto'
+}
+
+//Objeto novo tendo como protótipo o objeto pai
+const filha1 = Object.create(pai)
+
+//Editando o objeto filha01
+filha1.nome = "Julia"
+
+//Exibição
+console.log(filha1.corCabelo)
+console.log()
+
+//Objeto novo tendo como protótipo o objeto pai, com acrescimo de funções próprias no parametro
+const filha2 = Object.create(pai, {
+    nome: {
+        value: 'Bianca',
+        writable: false,
+        enumerable: true
     }
-}
+})
 
-const ferrari = {
-    modelo: 'F40',
-    velMax: 324   
-}
+//Exibindo dados
+console.log(`${filha2.nome} tem cabelo ${filha2.corCabelo}`)
+console.log()
 
+console.log(Object.keys(filha1))
+console.log(Object.keys(filha2))
+console.log()
 
-const volvo = {
-    modelo: 'V40',
-    status(){
-        
-    }  
+/*Verificar se determinada função pertence ao objeto ou se é herança.
+O termo "hasOwnProperty" serve para perguntar se determinada propriedade
+pertence a filha2.
+No retorno do console é mostrado que nome pertence ao objeto e 
+corCabelo é um atributo herdado.*/
+for (const key in filha2) {
+    filha2.hasOwnProperty(key) ?
+        console.log(key) : console.log(`Por herança: ${key}`)
 }
