2fa82bcce01270511616009efd89f4c92042e2e8
Adison Soares <62489704+AdisonSoares@users.noreply.github.com>
2022-02-27 14:14:02 -0300

    1_Fundamentos - 1_Efeito icamento.

--- Estudo/1_JavaScript/1_Fundamentos/Hoisting.js
+++ Estudo/1_JavaScript/1_Fundamentos/Hoisting.js
@@ -1,5 +1,5 @@
 /*Significa jogar para cima, não precisou definir antecipadamento
-**a variável "a", ela foi reconhecida e atribuida à ela o valor 
+**a variável "a", ela foi reconhecida e atribuida para ela o valor 
 **undefined.
 **O efeito de içamento não ocorre com o Let.*/
 
