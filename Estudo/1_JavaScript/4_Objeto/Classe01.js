b9a14f391c8347ea92a7349b82f6141e525b26fd
Adison Soares <adison.soares7@gmail.com>
2022-02-27 13:01:01 -0300

    4_Objeto - 1_Classe.

---
+++ Estudo/1_JavaScript/4_Objeto/Classe01.js
@@ -0,0 +1,48 @@
+/*Exemplos de classes.
+Todas as classes são um tipo de função.*/
+
+class Lancamento {
+    //Parametros da função
+    constructor(nome = 'Generico', valor = 0) {
+        this.nome = nome
+        this.valor = valor
+    }
+}
+
+class CicloFinanceiro {
+    //Parametros da função
+    constructor(mes, ano) {
+        this.mes = mes
+        this.ano = ano
+
+        //Incluindo um array
+        this.lancamentos = []
+    }
+    //Adicionando funções - OBS: Os 3 pontos indicam que será um array no parametro variados
+    addLancamentos(...lancamentos) {
+        //Armazenando cada um dos elementos, "lancamento", do array,
+        lancamentos.forEach(lancamento => this.lancamentos.push(lancamento))
+    }
+    //Adicionando funções
+    sumario() {
+        let valorConsolidado = 0
+        //Para cada um dos lançamentos irá somar com consolidado
+        this.lancamentos.forEach(lancamento => {
+            valorConsolidado += lancamento.valor
+        })
+        return valorConsolidado
+    }
+}
+
+//Testando a classe Lançamento
+const salario = new Lancamento('Salario', 45000)
+const contaDeLuz = new Lancamento('Luz', -220)
+
+//Testando a classe Ciclo Financeiro
+const contas = new CicloFinanceiro(6, 2018)
+
+//Adicionando valores
+contas.addLancamentos(salario, contaDeLuz)
+
+//Exibindo dados
+console.log(contas.sumario())
