3c373f59f3ee6abdf97701fa669a9d4410c03af8
Adison Soares <62489704+AdisonSoares@users.noreply.github.com>
2022-02-27 14:20:17 -0300

    1_Fundamentos - 1_Organizacao sentenca.

--- Estudo/1_JavaScript/1_Fundamentos/Organizacao.js
+++ Estudo/1_JavaScript/1_Fundamentos/Organizacao.js
@@ -1,6 +1,6 @@
 /*O JavaScript é organizado por sentenças de códigos, que podem
 ou não terminar com ponto e vírgula e também podem ser organizados em blocos
-de códigos que são limitados por chaves. */
+de códigos que são limitados por chaves.*/
 console.log("Sentença de código");
 {
     {
